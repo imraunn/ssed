eafefe1f76fe503857bf02f80a1ddf64120448ec
justanothern00b <asnaniraunak@gmail.com>
2021-02-27 20:30:32 +0530

    Data extraction user friendly now Added filters for post data processing

--- WebScraper/code_extract.py
+++ WebScraper/code_extract.py
@@ -7,21 +7,45 @@ import os
 
 f=open("database.txt","r")
 lines=f.read().splitlines()
-
+syntax=["import","echo"]
 data={}
 for lang in lines:
     url="https://esolangs.org/wiki/"+str(lang)
     page=requests.get(url)
     soup=BeautifulSoup(page.content,'html.parser')
+    counter=1
+    z=[]
     for sample in soup.findAll(['pre','code']):
-        print(urllib.parse.unquote(sample.text))
-        print('\n')
-        print(str(lang))
-        print("-----------------------")
-        val=input("Do you want to keep this? y/n ")
-        if val=="y" :
-            print("yos")
-            os.system('cls' if os.name == 'nt' else 'clear')
-        else:
-            print("gg")
-            os.system('cls' if os.name == 'nt' else 'clear')
+        t=urllib.parse.unquote(sample.text)
+        if len(t) > 10 and all(word not in str(t) for word in syntax):
+            z.append(str(urllib.parse.unquote(sample.text)))
+            """print(urllib.parse.unquote(sample.text))
+            print('\n')
+            print(str(lang))
+            print("-----------------------")"""
+    if len(z)>=1:
+        for j in z:
+            print()
+            print("Probable code ",counter)
+            print("-----------------------")
+            counter=counter+1
+            print(j[:150])
+            print("-----------------------")
+        print()
+        print("Language is", str(lang))
+        val=input("What all do you want to keep? Enter values of counter as a string: ")
+        for i in val:
+            k=int(i)
+            print(z[k-1][:150])
+            #implement storage in a file
+
+
+    """
+            val=input("Do you want to keep this? y/n ")
+            if val=="y" :
+                print("yos")
+                os.system('cls' if os.name == 'nt' else 'clear')
+            else:
+                print("gg")
+                os.system('cls' if os.name == 'nt' else 'clear')
+    """

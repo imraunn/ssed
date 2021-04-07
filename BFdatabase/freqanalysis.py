6b4feb6bd7421860c89fecb8ca3911043540834c
Raunak Asnani <asnaniraunak@gmail.com>
2021-04-07 22:09:21 +0530

    Space character issue fixed

--- BFdatabase/freqanalysis.py
+++ BFdatabase/freqanalysis.py
@@ -3,7 +3,7 @@ def charset_gen(input):
     storage=Counter(input)
     charsetlist=[]
     for element in storage:
-        if element not in [" ","\n"]:
+        if element not in ["\n"]:
             charsetlist.append(element)
     return charsetlist
 def wordlist_gen(input):
